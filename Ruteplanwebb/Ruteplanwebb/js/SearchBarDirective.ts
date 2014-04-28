75a565de27d97e2c7beb081493fb8164092360da
Peter Löfås <peter_lofas@hotmail.com>
2014-04-28 14:29:15 +0200

    Splittat MapDirective och SearchBar till egna filer

---
+++ Ruteplanwebb/Ruteplanwebb/js/SearchBarDirective.ts
@@ -0,0 +1,26 @@
+﻿///<reference path="../Scripts/typings/angularjs/angular.d.ts"/>
+///<reference path="app.ts"/>
+
+class SeachBarDirective {
+    restrict: string
+    replace: boolean;
+    template: string;
+    link: any;
+    map: L.Map;
+
+    constructor() {
+        this.restrict = 'E';
+        this.replace = true;
+        this.template = '<div><div><b>Ruteplan demo</b><br/>' +
+        'Fra: <input type="text" ng-model="selectedFrom"  id="searchFrom" typeahead="address.name for address in getLocations($viewValue) | filter:$viewValue" typeahead-leading="loadingAdressFrom" typeahead-on-select="onSelectFrom($item,$model,$label)"/><i ng-show="loadingAdressFrom" class="glyphicon glyphicon-refresh"></i> Til: <input type="text"  ng-model="selectedTo"  typeahead="address.name for address in getLocations($viewValue) | filter:$viewValue" typeahead-leading="loadingAdressTo" typeahead-on-select="onSelectTo($item,$model,$label)" id="searchTo"/><i ng-show="loadingAdressTo" class="glyphicon glyphicon-refresh"></i>' +
+        '<input id="searchbutton" type="button" value="Søk" ng-click="doRouteCalculation()"/></div></div>';
+        this.link = function (scope: any, element: any, attrs: any) {
+
+        }
+    }
+}
+
+
+svvRuteplanApp.directive('searchbar', function () {
+    return new SeachBarDirective();
+});
