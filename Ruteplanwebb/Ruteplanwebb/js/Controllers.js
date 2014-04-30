301725e24974c998d9368cf48c9d31dfbf991ca9
Stian Østerhaug <stian.osterhaug@triona.no>
2014-04-30 11:38:10 +0200

    Remove compiled js files

--- Ruteplanwebb/Ruteplanwebb/js/Controllers.js
+++
@@ -1,3 +0,0 @@
-///<reference path="MapController.ts"/>
-var svvRuteplanControllers = angular.module('svvRuteplanControllers', []).controller("MapController", ["$scope", "$http", MapController]);
-//# sourceMappingURL=Controllers.js.map
--- Ruteplanwebb/Ruteplanwebb/js/app.js
+++
@@ -1,8 +0,0 @@
-/// <reference path="../Scripts/typings/angularjs/angular.d.ts"/>
-/// <reference path="../Scripts/typings/angularjs/angular-route.d.ts"/>
-var svvRuteplanApp = angular.module("svvRuteplanApp", ['ngRoute', 'svvRuteplanControllers', "ui.bootstrap"]).config([
-    '$routeProvider', function ($routeprovider) {
-        $routeprovider.when('/map', { templateUrl: 'Views/MapView.html', controller: 'MapController' });
-        $routeprovider.otherwise({ redirectTo: '/map' });
-    }]);
-//# sourceMappingURL=app.js.map
