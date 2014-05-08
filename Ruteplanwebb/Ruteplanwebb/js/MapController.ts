54e388cfb424eea1eb785aaa730342c68f3bba6d
Stian Østerhaug <stian.osterhaug@triona.no>
2014-05-08 14:56:45 +0200

    Calculate route and clean up dead code.

--- Ruteplanwebb/Ruteplanwebb/js/MapController.ts
+++ Ruteplanwebb/Ruteplanwebb/js/MapController.ts
@@ -15,15 +15,17 @@ interface IMapControllerScope extends ng.IScope {
     doRouteCalculation();
     getRoute(fromX: number, fromY: number, toX: number, toY: number);
     updateMarkers();
-    updateMarkers2();
-    markerPoints: AddressItem[];
-    fromPoint: AddressItem;
-    toPoint: AddressItem;
-    map: L.Map;
-    selectedFrom: any;
+    map: OpenLayers.Map;
     fromAddress: AddressItem;
     toAddress: AddressItem;
     markers: OpenLayers.Layer.Markers;
+}
+
+interface BoundingBox {
+    xmin: number;
+    ymin: number;
+    xmax: number;
+    ymax: number;
 }
 
 /* The MapController, holds functionality for the map implementation (autocomplete, searching, routing,...)*/
@@ -34,45 +36,23 @@ class MapController {
             return this.getLocationsSk($scope, val, $http);
         };
 
-//        $scope.doRouteCalculation = () => {
-//
-//            var fromX = $scope.fromPoint.location.lng;
-//            var fromY = $scope.fromPoint.location.lat;
-//            var toX = $scope.toPoint.location.lng;
-//            var toY = $scope.toPoint.location.lat;
-//
-//            if ($scope.map.options.crs != null) {
-//                var pt = $scope.map.options.crs.project($scope.fromPoint.location);
-//                fromX = pt.x;
-//                fromY = pt.y;
-//                pt = $scope.map.options.crs.project($scope.toPoint.location);
-//                toX = pt.x;
-//                toY = pt.y;
-//            }
-//
-//            $scope.getRoute(fromX, fromY, toX, toY);
-//        };
-
-        /* Does route calculation */
-        $scope.getRoute = (fromX: number, fromY: number, toX: number, toY: number) => {
+        $scope.doRouteCalculation = () => {
+            var fromX = $scope.fromAddress.location.lon;
+            var fromY = $scope.fromAddress.location.lat;
+            var toX = $scope.toAddress.location.lon;
+            var toY = $scope.toAddress.location.lat;
             $http.get('routingService', {
                 params: {
                     stops: fromX + "," + fromY + ";" + toX + "," + toY,
                     format: "json"
                 }
             }).success((data: any) => {
-                var bbox = data.directions[0].summary.envelope;
-                var sw = $scope.map.options.crs.projection.unproject(new L.Point(bbox.xmin, bbox.ymin));
-                var ne = $scope.map.options.crs.projection.unproject(new L.Point(bbox.xmax, bbox.ymax));
-                $scope.map.fitBounds(L.latLngBounds(sw, ne));
+                var bbox = <BoundingBox>data.directions[0].summary.envelope;
+                $scope.map.zoomToExtent([bbox.xmin, bbox.ymin, bbox.xmax, bbox.ymax]);
             });
         };
 
-        $scope.updateMarkers2 = () => {
-            console.log("UpdateMarkers2");
-            console.log($scope.fromAddress.location);
-            console.log($scope.toAddress);
-
+        $scope.updateMarkers = () => {
             $scope.markers.clearMarkers();
 
             var size = new OpenLayers.Size(21, 25);
@@ -96,44 +76,26 @@ class MapController {
             eksakteForst: true
         }
     }).then(function(xmlRes) {
-
         var x2Js = new X2JS();
         var res = x2Js.xml_str2json(xmlRes.data);
-        var addresses = [];
+        var addresses = new Array<AddressItem>();
 
-        if (angular.isArray(res.sokRes.stedsnavn)) {
-            angular.forEach(res.sokRes.stedsnavn, function(item) {
-                var location = new OpenLayers.LonLat([parseFloat(item.aust), parseFloat(item.nord)]);
-                var name = item.stedsnavn + ", " + item.fylkesnavn + " (" + item.navnetype + ")";
-                var address = new AddressItem(name, location);
-                addresses.push(address);
-            });
-        } else if (res.sokRes.stedsnavn != null) {
-            var item = res.sokRes.stedsnavn;
+        var add = (item: any) => {
             var location = new OpenLayers.LonLat([parseFloat(item.aust), parseFloat(item.nord)]);
             var name = item.stedsnavn + ", " + item.fylkesnavn + " (" + item.navnetype + ")";
             var address = new AddressItem(name, location);
             addresses.push(address);
+        };
+
+        if (angular.isArray(res.sokRes.stedsnavn)) {
+            angular.forEach(res.sokRes.stedsnavn, function(item) {
+                add(item);
+            });
+        } else if (res.sokRes.stedsnavn != null) {
+            add(res.sokRes.stedsnavn);
         }
+
         return addresses;
     });
 
-    /* Does return locations to the autocomplete boxes for from and to place */
-    getLocationsGoogle = ($scope, val, $http: ng.IHttpService) => $http.get('http://maps.googleapis.com/maps/api/geocode/json', {
-        params: {
-            address: val,
-            sensor: false,
-            language: "no",
-            region: "NO",
-            components: "country:NO"
-        }
-    }).then(res => {
-        var addresses = [];
-        angular.forEach(res.data.results, item => {
-            if (item.geometry != null && item.geometry.location != null) {
-                addresses.push({ name: item.formatted_address, location: item.geometry.location });
-            }
-        });
-        return addresses;
-    });
 }
