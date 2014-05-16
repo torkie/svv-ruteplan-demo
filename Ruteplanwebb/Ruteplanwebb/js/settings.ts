3a8fb4ca71f29a4242cde60c9b0f8e9f0dec29d3
Stian Østerhaug <stian.osterhaug@triona.no>
2014-05-16 14:39:00 +0200

    Put settings in a service

--- Ruteplanwebb/Ruteplanwebb/js/settings.ts
+++ Ruteplanwebb/Ruteplanwebb/js/settings.ts
@@ -1,9 +1,7 @@
 ///<reference path="../ts/typings/angularjs/angular.d.ts"/>
 
 angular.module("rpwSettings", ["ngCookies"])
-    .controller("SettingsController", ["$scope", "$modal", "$cookies", function($scope, $modal, $cookies) {
-
-        console.log($cookies);
+    .controller("SettingsController", ["$scope", "$modal", "$cookies", "settings", function($scope, $modal, $cookies, settings) {
 
         $scope.open = function(size) {
             var modalInstance = $modal.open({
@@ -32,7 +30,35 @@ angular.module("rpwSettings", ["ngCookies"])
             });
         };
 
-    }]);
+    }])
+    .factory("settings", function($cookies) {
+        var settings = {};
+
+        console.log("Cookies from settings service:");
+        console.log($cookies);
+
+        if ($cookies.url === undefined) {
+            settings["url"] = "http://default.com";
+        } else {
+            settings["url"] = $cookies.url;
+        }
+        if ($cookies["username"] === undefined) {
+            settings["username"] = "user";
+        } else {
+            settings["username"] = $cookies.username;
+        }
+        if ($cookies["password"] === undefined) {
+            settings["password"] = "pass";
+        } else {
+            settings["password"] = $cookies.password;
+        }
+
+        settings["save"] = function() {
+            console.log("save settings");
+        };
+
+        return settings;
+    });
 
 var ModalInstanceCtrl = function($scope, $modalInstance, data) {
     $scope.data = data;
