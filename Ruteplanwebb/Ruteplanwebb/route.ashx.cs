35fe8811bbf85a175b45c3df61f476764a27a44c
Stian Østerhaug <stian.osterhaug@triona.no>
2014-05-20 09:05:50 +0200

    code cleanup

--- Ruteplanwebb/Ruteplanwebb/route.ashx.cs
+++ Ruteplanwebb/Ruteplanwebb/route.ashx.cs
@@ -23,31 +23,34 @@ namespace Ruteplanwebb
             var request = context.Request;
 
             // kinda a hack to copy request.QueryString so we get a mutable instance
-            var copy = HttpUtility.ParseQueryString(request.QueryString.ToString());
+            var queryString = HttpUtility.ParseQueryString(request.QueryString.ToString());
 
             // get proxy parameters
-            var backend_url = copy["backend_url"];
-            var backend_username = copy["backend_username"];
-            var backend_password = copy["backend_password"];
+            var backend_url = queryString["backend_url"];
+            var backend_username = queryString["backend_username"];
+            var backend_password = queryString["backend_password"];
 
             // remove parameters that shouldn't be passed to backend
-            copy.Remove("backend_url");
-            copy.Remove("backend_username");
-            copy.Remove("backend_password");
+            queryString.Remove("backend_url");
+            queryString.Remove("backend_username");
+            queryString.Remove("backend_password");
 
             string url;
             if (String.IsNullOrEmpty(backend_url))
             {
-                url = "http://multirit.triona.se/routingService_v1_0/routingService?" + copy.ToString();
+                url = "http://multirit.triona.se/routingService_v1_0/routingService?" + queryString.ToString();
             }
             else
             {
                 var uri = new Uri(backend_url);
                 var hostname = uri.Host;
+                // "security check"
                 var regex = new Regex("(\\.vegvesen\\.no|\\.triona\\.se)$");
-                if (regex.IsMatch(hostname)) {
-                    url = backend_url + "?" + copy.ToString();
-                } else
+                if (regex.IsMatch(hostname))
+                {
+                    url = backend_url + "?" + queryString.ToString();
+                }
+                else
                 {
                     context.Response.StatusCode = (int)HttpStatusCode.BadRequest;
                     context.Response.End();
@@ -57,6 +60,7 @@ namespace Ruteplanwebb
 
             var wq = WebRequest.Create(url);
 
+            // add basic auth header if we have username
             if (!String.IsNullOrEmpty(backend_username))
             {
                 var cred = Encoding.UTF8.GetBytes(backend_username + ":" + backend_password);
