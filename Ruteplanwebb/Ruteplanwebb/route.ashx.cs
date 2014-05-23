85428d7ccdffa1fb80d20c87095cf532aaf7d7f0
Peter Löfås <peter_lofas@hotmail.com>
2014-05-23 23:58:25 +0200

    fixed proxy and added missing ts-file

--- Ruteplanwebb/Ruteplanwebb/route.ashx.cs
+++ Ruteplanwebb/Ruteplanwebb/route.ashx.cs
@@ -1,23 +1,14 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.Sockets;
-using System.Security.Cryptography.X509Certificates;
-using System.Web.UI;
-using System.Globalization;
 using System.Net;
-using System.Web;
-using System.Web.Script.Serialization;
-using System.Xml.Serialization;
 using System.Text.RegularExpressions;
-using System.Text;
+using System.Web;
 
 namespace Ruteplanwebb
 {
     /// <summary>
     /// Summary description for route
     /// </summary>
-    public class Route : IHttpHandler
+    public class Route : BaseProxy, IHttpHandler
     {
         public void ProcessRequest(HttpContext context)
         {
@@ -27,9 +18,9 @@ namespace Ruteplanwebb
             var queryString = HttpUtility.ParseQueryString(request.QueryString.ToString());
 
             // get proxy parameters
-            var backend_url = queryString["backend_url"];
-            var backend_username = queryString["backend_username"];
-            var backend_password = queryString["backend_password"];
+            var backendUrl = queryString["backend_url"];
+            var backendUsername = queryString["backend_username"];
+            var backendPassword = queryString["backend_password"];
 
             // remove parameters that shouldn't be passed to backend
             queryString.Remove("backend_url");
@@ -37,46 +28,29 @@ namespace Ruteplanwebb
             queryString.Remove("backend_password");
 
             string url;
-            if (String.IsNullOrEmpty(backend_url))
+            if (String.IsNullOrEmpty(backendUrl))
             {
-                url = "http://multirit.triona.se/routingService_v1_0/routingService?" + queryString.ToString();
+                url = "http://multirit.triona.se/routingService_v1_0/routingService?" + queryString;
             }
             else
             {
-                var uri = new Uri(backend_url);
+                var uri = new Uri(backendUrl);
                 var hostname = uri.Host;
                 // "security check"
                 var regex = new Regex("(\\.vegvesen\\.no|\\.triona\\.se)$");
                 if (regex.IsMatch(hostname))
                 {
-                    url = backend_url + (!backend_url.EndsWith("?", StringComparison.Ordinal) ? "?" : "") + queryString;
+                    url = backendUrl + (!backendUrl.EndsWith("?", StringComparison.Ordinal) ? "?" : "") + queryString;
                 }
                 else
                 {
-                    context.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+                    context.Response.StatusCode = (int) HttpStatusCode.BadRequest;
                     context.Response.End();
                     return;
                 }
             }
 
-            var wq = WebRequest.Create(url) as HttpWebRequest;
-            // add basic auth header if we have username
-            if (!String.IsNullOrEmpty(backend_username))
-            {
-                wq.PreAuthenticate = true;
-                wq.Credentials = new NetworkCredential(backend_username, backend_password);
-            }
-
-            using (var resp = wq.GetResponse())
-            {
-                using (var respstream = resp.GetResponseStream())
-                {
-                    if (respstream != null)
-                    {
-                        respstream.CopyTo(context.Response.OutputStream);
-                    }
-                }
-            }
+            ProxyRequest(context, url, backendUsername, backendPassword);
         }
 
         public bool IsReusable
@@ -84,6 +58,79 @@ namespace Ruteplanwebb
             get
             {
                 return false;
+            }
+        }
+    }
+
+    public class WmsProxy : BaseProxy, IHttpHandler
+    {
+        public void ProcessRequest(HttpContext context)
+        {
+            var request = context.Request;
+            // kinda a hack to copy request.QueryString so we get a mutable instance
+            var queryString = HttpUtility.ParseQueryString(request.QueryString.ToString());
+
+            // get proxy parameters
+            var backendUrl = queryString["url"];
+            var backendUsername = queryString["backend_username"];
+            var backendPassword = queryString["backend_password"];
+
+            // remove parameters that shouldn't be passed to backend
+            queryString.Remove("url");
+            queryString.Remove("backend_username");
+            queryString.Remove("backend_password");
+
+            string url;
+            if (String.IsNullOrEmpty(backendUrl))
+            {
+                throw new NotSupportedException("No URL Given");
+            }
+
+
+            url = backendUrl + (!backendUrl.EndsWith("?", StringComparison.Ordinal) ? "?" : "") + "REQUEST=GetCapabilities&SERVICE=WMS";
+
+            ProxyRequest(context, url, backendUsername, backendPassword);
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+
+    public class BaseProxy
+    {
+        public static void ProxyRequest(HttpContext context, string url, string backendUsername, string backendPassword)
+        {
+            var wq = WebRequest.Create(url) as HttpWebRequest;
+            // add basic auth header if we have username
+            if (!String.IsNullOrEmpty(backendUsername))
+            {
+                if (wq != null)
+                {
+                    wq.PreAuthenticate = true;
+                    wq.Credentials = new NetworkCredential(backendUsername, backendPassword);
+                }
+            }
+
+            if (wq == null)
+            {
+                return;
+            }
+
+            using (var resp = wq.GetResponse())
+            {
+                context.Response.ContentType = resp.ContentType;
+                using (var respstream = resp.GetResponseStream())
+                {
+                    if (respstream != null)
+                    {
+                        respstream.CopyTo(context.Response.OutputStream);
+                    }
+                }
             }
         }
     }
